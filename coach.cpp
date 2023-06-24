abeaa834918484d7bd6bdeefd580cd8e73328be8
deionjsm <deionjsm@gmail.com>
2023-06-24 18:54:47 -0400

    debgugging, cleaning input

--- coach.cpp
+++ coach.cpp
@@ -122,10 +122,12 @@ void Coach::view_schedule()
                 // Extract the ID after "Player ID: "
                 int player_id = std::stoi(player_id_str.substr(11));
 
-                std::string extracted_membership_type;
-                if(this->get_membership().length() > 18){
-                   extracted_membership_type = this->get_membership().substr(18);
-                }
+                // Assuming the membership type is second in the formatted string
+                std::string membership_type_str;
+                std::getline(ss, membership_type_str, ',');
+                std::cout << membership_type_str << std::endl;
+                // Extract the membership type after "Membership Type: "
+                std::string extracted_membership_type = membership_type_str.substr(17);
 
                 // If the player ID matches the current member's ID and the membership type matches the given type, print the line
                 if (player_id == current_player_id && extracted_membership_type == this->get_membership()) {
